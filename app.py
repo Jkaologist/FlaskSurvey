c2709851c91ef53703b12abbe34aceae5b515f36
John <figurefactory@gmail.com>
2021-03-04 14:56:00 -0500

    got questions started

--- app.py
+++ app.py
@@ -7,3 +7,17 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+responses = []
+
+curr_question = 0
+
+@app.route("/")
+def index():
+
+    return render_template('survey_start.html', survey = survey)
+
+@app.route("/begin")
+def question_render():
+
+    return render_template('question.html', question = survey.questions[curr_question])
