0b2eafa3fe62a6945ed120ed9eba6371129d6f52
John <figurefactory@gmail.com>
2021-03-04 20:32:57 -0500

    switched to session storage for answers

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
-responses = []
 
 @app.route("/")
 def index():
@@ -16,18 +15,20 @@ def index():
 
 @app.route("/begin", methods=["GET", "POST"])
 def question_render():
+    session["responses"] = []
     return redirect('/questions/0')
 
 @app.route("/questions/<int:num>")
 def question_template(num):
-    global responses
-    flash(responses)
-    if len(responses) == len(survey.questions):
-        responses = [] #take this out later
+    flash(session['responses'])
+    if len(session['responses']) == len(survey.questions):
         return render_template('completion.html')
-    return render_template('question.html', question=survey.questions[len(responses)])
+    return render_template('question.html', question=survey.questions[len(session['responses'])])
 
 @app.route("/answer", methods=["GET", "POST"])
 def question_responses():
+    responses = session['responses']
     responses.append(request.form['answer'])
+    session['responses'] = responses
     return redirect(f'questions/{len(responses)}')
+
